ba959cda9a8bde306592a2b1d63830c43b4dcdb4
Dai Nguyen <30742122+ddajing@users.noreply.github.com>
2019-12-13 16:31:28 +0700

    Update ModInt.cpp

--- Math/ModInt.cpp
+++ Math/ModInt.cpp
@@ -16,3 +16,43 @@ struct ModInt {
   ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
 };
 typedef ModInt<1000000007> mint;
+
+template<class T> struct modular {
+	T val; 
+	explicit operator T() const { return val; }
+	modular() { val = 0; }
+	modular(const ll& v) { 
+		val = (-MOD <= v && v <= MOD) ? v : v % MOD;
+		if (val < 0) val += MOD;
+	}
+	 //-----------------------------------------------------------------------------------
+	// friend ostream& operator<<(ostream& os, const modular& a) { return os << a.val; }
+	friend void pr(const modular& a) { pr(a.val); }
+	friend void re(modular& a) { ll x; re(x); a = modular(x); }
+   
+	friend bool operator==(const modular& a, const modular& b) { return a.val == b.val; }
+	friend bool operator!=(const modular& a, const modular& b) { return !(a == b); }
+	friend bool operator<(const modular& a, const modular& b) { return a.val < b.val; }
+ 
+	modular operator-() const { return modular(-val); }
+	modular& operator+=(const modular& m) { if ((val += m.val) >= MOD) val -= MOD; return *this; }
+	modular& operator-=(const modular& m) { if ((val -= m.val) < 0) val += MOD; return *this; }
+	modular& operator*=(const modular& m) { val = (ll)val*m.val%MOD; return *this; }
+	friend modular pow(modular a, ll p) {
+		modular ans = 1; for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+		return ans;
+	}
+	friend modular inv(const modular& a) { 
+		auto i = invGeneral(a.val,MOD); assert(i != -1);
+		return i;
+	} // equivalent to return exp(b,MOD-2) if MOD is prime
+	modular& operator/=(const modular& m) { return (*this) *= inv(m); }
+	
+	friend modular operator+(modular a, const modular& b) { return a += b; }
+	friend modular operator-(modular a, const modular& b) { return a -= b; }
+	friend modular operator*(modular a, const modular& b) { return a *= b; }
+	
+	friend modular operator/(modular a, const modular& b) { return a /= b; }
+};
+ 
+typedef modular<int> mi;
